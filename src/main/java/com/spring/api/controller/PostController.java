e73a594ccfe29a598768f90acfde542c4672c91f
94-c <hyeongwoo26@gmail.com>
2022-10-19 16:32:58 +0900

    Controller 작성

---
+++ src/main/java/com/spring/api/controller/PostController.java
@@ -0,0 +1,48 @@
+package com.spring.api.controller;
+
+import com.spring.api.data.dto.PostDto;
+import com.spring.api.data.dto.PostResponse;
+import com.spring.api.service.PostService;
+import com.spring.api.util.AppConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping()
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping("/api/v1/posts")
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto dto) {
+        return new ResponseEntity<>(postService.createPost(dto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/v1/posts")
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+    ) {
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id){
+        PostDto postResponse = postService.updatePost(postDto, id);
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post entity deleted successfully.", HttpStatus.OK);
+    }
+
+}
